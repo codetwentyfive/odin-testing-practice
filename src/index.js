8be6ced8e90d525242c542b03e32482f1fa23d6a
Code25 <chingisenkhbaatar@gmail.com>
2023-12-05 19:29:03 +0100

    added webpack

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import _ from 'lodash';
 function component() {
     const element = document.createElement('div');
     //Lodash,now imported by this script  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'world'], ' ');
 
     return element;
 }
